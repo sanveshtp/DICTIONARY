2df7cc94deb3223664b7cfcc281a2b09170a00c3
sanveshtp <sanveshthumkunta@gmail.com>
2024-02-02 20:04:13 +0530

    javascript changes

--- script.js
+++ script.js
@@ -14,14 +14,12 @@ dabba.addEventListener("keydown", (e) => {
     }
 })
 
-
 let getdata = async (word) => {
     let bd = document.body
     peddadabba.innerHTML = null;
     let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
     let data = await response.json()
-    console.log(data);
-    // let meanings = data[0]
+    // console.log(data);
     let cnt = 0
     while (cnt < 4) {
         if ((data[0].meanings[0].partOfSpeech) == undefined) {
@@ -52,16 +50,10 @@ let getdata = async (word) => {
     }
     peddadabba.innerHTML += `<b>Phonetic: </b>${data[0].phonetic}<br>`
 
-    // console.log(data[0].phone);
-    // peddadabba.innerHTML += `<b>Phonetic: </b>${data[0].phonetic}<br>`
-
-
-
-
     // SYNONYMS 0
     cnt = 0;
     if (data[0].meanings[0].synonyms.length != 0 || data[0].meanings[1].synonyms.length != 0) {
-        if (data[0].meanings[0].synonyms[1] !== undefined || data[0].meanings[1].synonyms[1] !== undefined) {
+        if (data[0].meanings[0].synonyms[1] !== undefined) {
             peddadabba.innerHTML += `<b>Synonyms: </b>`
             while (cnt < 2) {
                 if (cnt < 1) {
@@ -75,8 +67,10 @@ let getdata = async (word) => {
 
         }
 
-        if (data[0].meanings[1].synonyms[1] !== undefined) {
+        else if (data[0].meanings[1].synonyms[1] !== undefined) {
             // SYNONYMS 1
+            peddadabba.innerHTML += `<b>Synonyms: </b>`
+
             cnt = 0;
             while (cnt < 2) {
                 if (cnt < 1) {
@@ -101,7 +95,7 @@ let getdata = async (word) => {
     console.log(data[0].meanings[0].antonyms.length);
     if (data[0].meanings[0].antonyms.length != 0 || data[0].meanings[1].antonyms.length != 0) {
 
-        if (data[0].meanings[0].antonyms[1] != undefined || data[0].meanings[1].antonyms[1] != undefined) {
+        if (data[0].meanings[0].antonyms[1] != undefined) {
             peddadabba.innerHTML += `<b>Antonyms: </b>`
             while (cnt < 2) {
                 if (cnt < 1) {
@@ -113,8 +107,9 @@ let getdata = async (word) => {
                 }
             }
         }
-        if (data[0].meanings[1].antonyms[1] != undefined) {
+        else if (data[0].meanings[1].antonyms[1] != undefined) {
             // ANTONYMS 1
+            peddadabba.innerHTML += `<b>Antonyms: </b>`
             cnt = 0;
             while (cnt < 2) {
                 if (cnt < 1) {
