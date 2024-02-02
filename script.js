e574a18d3aad3a81aa0622e827739b653cb5b37d
sanveshtp <sanveshthumkunta@gmail.com>
2024-02-02 17:53:51 +0530

    nouns

--- script.js
+++ script.js
@@ -14,8 +14,6 @@ dabba.addEventListener("keydown", (e) => {
 })
 
 
-
-
 let getdata = async (word) => {
     let bd = document.body
 
@@ -23,27 +21,57 @@ let getdata = async (word) => {
     peddadabba.innerHTML = null;
     let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
     let data = await response.json()
-    let meanings = data[0].meanings[0].definitions
-    let cnt = 1
-    for (let i of meanings) {
+    console.log(data);
+    // let meanings = data[0]
+    let cnt = 0
+    while (cnt < 4) {
         if (cnt < 3) {
-            peddadabba.innerHTML += `<b>Definition ${String(cnt)}</b>: ${i.definition}<br>`
+            // console.log(data.length);
+            // console.log(data[0].meanings[cnt].definitions[cnt].definition);
+            peddadabba.innerHTML += `<b>${data[0].meanings[cnt].partOfSpeech} </b>: ${data[0].meanings[cnt].definitions[cnt].definition}<br>`
             cnt++
         } else {
             break
         }
     }
-    peddadabba.innerHTML += `<b>Synonyms :</b>`
-    synonyms = data[0].meanings[0].synonymsc
-    cnt = 1
-    for (let i of synonyms) {
-        if (cnt < 2) {
-            peddadabba.innerHTML += `<br>${i}`
+    cnt = 0;
+    if (data[0].meanings[cnt].antonyms[cnt] != undefined) {
+        peddadabba.innerHTML += `<b>Synonyms: </b>`
+
+    }
+    while (cnt < 2) {
+        if (data[0].meanings[cnt].synonyms[cnt] === undefined) {
+            break
+        }
+        if (cnt < 1) {
+            peddadabba.innerHTML += `${data[0].meanings[cnt].synonyms[cnt]},`
             cnt++
         } else {
+            peddadabba.innerHTML += `${data[0].meanings[cnt].synonyms[cnt]}<br>`
             break
         }
     }
+
+    cnt = 0;
+    // console.log(cnt);
+    if (data[0].meanings[cnt].antonyms[cnt] != undefined) {
+        peddadabba.innerHTML += `<b>Antonyms: </b>`
+    }
+    while (cnt < 2) {
+        if (data[0].meanings[cnt].antonyms[cnt] === undefined) {
+            break
+        }
+        if (cnt < 1) {
+            peddadabba.innerHTML += `${data[0].meanings[cnt].antonyms[cnt]},`
+            cnt++
+        } else {
+            peddadabba.innerHTML += `${data[0].meanings[cnt].antonyms[cnt]}<br>`
+            break
+        }
+    }
+    peddadabba.innerHTML += `<b>Phonetic: </b>${data[0].phonetic}<br>`
+
+
 
 
 }
