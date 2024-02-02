2448fd33be2fc381b7cdc4ee6626eabbe566cec7
sanveshtp <sanveshthumkunta@gmail.com>
2024-02-02 20:38:53 +0530

    response.status

--- script.js
+++ script.js
@@ -16,108 +16,115 @@ dabba.addEventListener("keydown", (e) => {
 let getdata = async (word) => {
     peddadabba.innerHTML = null;
     let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-    let data = await response.json()
-    let cnt = 0
-    while (cnt < 4) {
-        if ((data[0].meanings[0].partOfSpeech) == undefined) {
-            break
-        }
-        if (cnt < 1) {
-            // console.log(data.length);
-            // console.log(data[0].meanings[cnt].definitions[cnt].definition);
-            peddadabba.innerHTML += `<b>${data[0].meanings[0].partOfSpeech} </b>: ${data[0].meanings[0].definitions[cnt].definition}<br>`
-            cnt++
-        } else {
-            break
-        }
-    }
-    cnt = 0
-    while (cnt < 4) {
-        if ((data[0].meanings[1].partOfSpeech) == undefined) {
-            break
-        }
-        if (cnt < 1) {
-            // console.log(data.length);
-            // console.log(data[0].meanings[cnt].definitions[cnt].definition);
-            peddadabba.innerHTML += `<b>${data[0].meanings[1].partOfSpeech} </b>: ${data[0].meanings[1].definitions[cnt].definition}<br>`
-            cnt++
-        } else {
-            break
-        }
-    }
-    peddadabba.innerHTML += `<b>Phonetic: </b>${data[0].phonetic}<br>`
-
-    // SYNONYMS 0
-    cnt = 0;
-    if (data[0].meanings[0].synonyms.length != 0 || data[0].meanings[1].synonyms.length != 0) {
-        if (data[0].meanings[0].synonyms[1] !== undefined) {
-            peddadabba.innerHTML += `<b>Synonyms: </b>`
-            while (cnt < 2) {
-                if (cnt < 1) {
-                    peddadabba.innerHTML += `${data[0].meanings[0].synonyms[cnt]},`
-                    cnt++
-                } else {
-                    peddadabba.innerHTML += `${data[0].meanings[0].synonyms[cnt]}`
-                    break
-                }
+    if (response.status > 400) {
+        peddadabba.innerHTML = "Sorry, there's no english word like that :(";
+    } else {
+        let data = await response.json()
+        let cnt = 0
+        while (cnt < 4) {
+            if ((data[0].meanings[0].partOfSpeech) == undefined) {
+                break
             }
-
-        }
-
-        else if (data[0].meanings[1].synonyms[1] !== undefined) {
-            // SYNONYMS 1
-            peddadabba.innerHTML += `<b>Synonyms: </b>`
-
-            cnt = 0;
-            while (cnt < 2) {
-                if (cnt < 1) {
-                    peddadabba.innerHTML += `${data[0].meanings[1].synonyms[cnt]},`
-                    cnt++
-                } else {
-                    peddadabba.innerHTML += `${data[0].meanings[1].synonyms[cnt]}`
-
-                    break
-                }
+            if (cnt < 1) {
+                // console.log(data.length);
+                // console.log(data[0].meanings[cnt].definitions[cnt].definition);
+                peddadabba.innerHTML += `<b>${data[0].meanings[0].partOfSpeech} </b>: ${data[0].meanings[0].definitions[cnt].definition}<br>`
+                cnt++
+            } else {
+                break
             }
         }
-        peddadabba.innerHTML += '<br>'
-    }
-
-
-
-    // ANTONYMS 0
-    cnt = 0;
-    console.log(data[0].meanings[0].antonyms.length);
-    if (data[0].meanings[0].antonyms.length != 0 || data[0].meanings[1].antonyms.length != 0) {
-        if (data[0].meanings[0].antonyms[1] != undefined) {
-            peddadabba.innerHTML += `<b>Antonyms: </b>`
-            while (cnt < 2) {
-                if (cnt < 1) {
-                    peddadabba.innerHTML += `${data[0].meanings[0].antonyms[cnt]},`
-                    cnt++
-                } else {
-                    peddadabba.innerHTML += `${data[0].meanings[0].antonyms[cnt]}`
+        if (data[0].meanings != null) {
+            cnt = 0
+            while (cnt < 4) {
+                if (data[0].meanings[1].partOfSpeech === undefined) {
                     break
                 }
-            }
-        }
-        else if (data[0].meanings[1].antonyms[1] != undefined) {
-            // ANTONYMS 1
-            peddadabba.innerHTML += `<b>Antonyms: </b>`
-            cnt = 0;
-            while (cnt < 2) {
                 if (cnt < 1) {
-                    peddadabba.innerHTML += `${data[0].meanings[1].antonyms[cnt]},`
+                    // console.log(data.length);
+                    // console.log(data[0].meanings[cnt].definitions[cnt].definition);
+                    peddadabba.innerHTML += `<b>${data[0].meanings[1].partOfSpeech} </b>: ${data[0].meanings[1].definitions[cnt].definition}<br>`
                     cnt++
                 } else {
-                    peddadabba.innerHTML += `${data[0].meanings[1].antonyms[cnt]}`
                     break
                 }
             }
         }
 
+        if (data[0].phonetic != undefined) {
+            peddadabba.innerHTML += `<b>Phonetic: </b>${data[0].phonetic}<br>`
+        }
 
+        // SYNONYMS 0
+        cnt = 0;
+        if (data[0].meanings[0].synonyms.length != 0 || data[0].meanings[1].synonyms.length != 0) {
+            if (data[0].meanings[0].synonyms[1] !== undefined) {
+                peddadabba.innerHTML += `<b>Synonyms: </b>`
+                while (cnt < 2) {
+                    if (cnt < 1) {
+                        peddadabba.innerHTML += `${data[0].meanings[0].synonyms[cnt]},`
+                        cnt++
+                    } else {
+                        peddadabba.innerHTML += `${data[0].meanings[0].synonyms[cnt]}`
+                        break
+                    }
+                }
+
+            }
+
+            else if (data[0].meanings[1].synonyms[1] !== undefined) {
+                // SYNONYMS 1
+                peddadabba.innerHTML += `<b>Synonyms: </b>`
+
+                cnt = 0;
+                while (cnt < 2) {
+                    if (cnt < 1) {
+                        peddadabba.innerHTML += `${data[0].meanings[1].synonyms[cnt]},`
+                        cnt++
+                    } else {
+                        peddadabba.innerHTML += `${data[0].meanings[1].synonyms[cnt]}`
+
+                        break
+                    }
+                }
+            }
+            peddadabba.innerHTML += '<br>'
+        }
+
+
+
+        // ANTONYMS 0
+        cnt = 0;
+        if (data[0].meanings[0].antonyms.length != 0 || data[0].meanings[1].antonyms.length != 0) {
+            if (data[0].meanings[0].antonyms[1] != undefined) {
+                peddadabba.innerHTML += `<b>Antonyms: </b>`
+                while (cnt < 2) {
+                    if (cnt < 1) {
+                        peddadabba.innerHTML += `${data[0].meanings[0].antonyms[cnt]},`
+                        cnt++
+                    } else {
+                        peddadabba.innerHTML += `${data[0].meanings[0].antonyms[cnt]}`
+                        break
+                    }
+                }
+            }
+            else if (data[0].meanings[1].antonyms[1] != undefined) {
+                // ANTONYMS 1
+                peddadabba.innerHTML += `<b>Antonyms: </b>`
+                cnt = 0;
+                while (cnt < 2) {
+                    if (cnt < 1) {
+                        peddadabba.innerHTML += `${data[0].meanings[1].antonyms[cnt]},`
+                        cnt++
+                    } else {
+                        peddadabba.innerHTML += `${data[0].meanings[1].antonyms[cnt]}`
+                        break
+                    }
+                }
+            }
+
+
+        }
     }
-
 
 }
